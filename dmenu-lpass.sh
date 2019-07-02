fd9e5ce9d6d84da66d0e622c39e0b1704e3ce7fd
Tin Švagelj <tin.svagelj@live.com>
2019-07-02 15:53:10 +0200

    Added username reminder notification.

--- dmenu-lpass.sh
+++ dmenu-lpass.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-
-selection=`echo -e $(lpass ls | awk -F '[/[]' '{print $(NF-1)"\\\n"}') | dmenu`
+selection=`echo -e $(lpass ls | awk -F '[/[]' '{print $(NF-1)"\\\n"}') | dmenu -p "Select account: "`
 
 if [ "$selection" != "" ]; then
+    name=`echo -e $(lpass show --name $selection)`
+    notify-send -i edit-paste "$name" `echo -e $(lpass show --username $selection)` &
     lpass show -c --password $selection &
 fi
