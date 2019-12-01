cf2a7f4fa9e21ad231340912b0e76fa613732fe5
caellian <tin.svagelj@live.com>
2019-12-01 10:06:17 +0100

    Added login dialogs.

--- dmenu-lpass.sh
+++ dmenu-lpass.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
-selection=`echo -e $(lpass ls | awk -F '[/[]' '{print $(NF-1)"\\\n"}') | dmenu -p "Select account: "`
+if ! lpass status -q; then
+    username=`zenity --entry --text="LastPass Username"`
+    if ! test -z "$username"; then
+        lpass login --trust $username
+    fi
+fi
 
-if [ "$selection" != "" ]; then
-    name=`echo -e $(lpass show --name $selection)`
-    notify-send -i edit-paste "$name" `echo -e $(lpass show --username $selection)` &
-    lpass show -c --password $selection &
+if lpass status -q; then
+    selection=`echo -e $(lpass ls | awk -F '[/[]' '{print $(NF-1)"\\\n"}') | dmenu -p "Select account: "`
+
+    if [ "$selection" != "" ]; then
+        name=`echo -e $(lpass show --name $selection)`
+        notify-send -i edit-paste "$name" `echo -e $(lpass show --username $selection)` &
+        lpass show -c --password $selection &
+    fi
 fi
